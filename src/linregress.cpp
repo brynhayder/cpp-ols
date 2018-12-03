c7ab83719ec95c2aac649e6abe53985ed4f562b4
bryn <4146799+brynhayder@users.noreply.github.com>
2018-12-03 17:11:30 +0000

    almost at a minimally working program, just some cleaning up to be done

--- src/linregress.cpp
+++ src/linregress.cpp
@@ -7,15 +7,166 @@
 #include <iomanip>
 #include <iostream>
 #include <numeric>
-#include <vector>
-
 #include <Eigen/Dense>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-double DEFAULT_LAMBDA_VALUE = 1.;
+double DEFAULT_LAMBDA_VALUE = 0;
+char DELIM = ',';
 
+
+
+// ----------------
+// Parse command line arguments
+
+struct command_line_args {
+    command_line_args() : lambda(DEFAULT_LAMBDA_VALUE) {}
+
+    double lambda;
+    std::string input_filename;
+
+};
+
+void print_help_message() {
+    std::cout << "help!" << std::endl;
+    return;
+}
+
+command_line_args parse_args(int argc, char *argv[]) {
+    const char *const short_opts = "l:h";
+    const option long_opts[] = {
+            {"lambda", required_argument, nullptr, 'l'},
+            {"help",   no_argument,       nullptr, 'h'},
+    };
+
+    command_line_args output;
+    int opt;
+    int option_index = 0;
+    while ((opt = getopt_long(argc, argv, short_opts, long_opts, &option_index)) != -1) {
+        switch (opt) {
+            case 'l':
+                output.lambda = std::stod(optarg);
+                break;
+
+            case 'h':
+            case '?':
+            default:
+                print_help_message();
+                exit(EXIT_SUCCESS);
+        }
+    }
+
+    // sort out the below!!!
+    if (optind < argc) { // IS THIS THE BEST WAY?
+        output.input_filename = argv[optind++];
+    } else {
+        std::cerr << "Failure: must give an input file." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (optind < argc) {
+        std::cerr << "Warning: " << argc - optind << " additional arguments given, all ignored!\n" << std::endl;
+    }
+
+    return output;
+}
+// ----------------
+
+
+
+// ----------------
+// Parse inputs into Eigen matrices
+
+struct data_size {
+    std::size_t n_features;
+    std::size_t n_examples;
+};
+
+struct input_data {
+    MatrixXd X;
+    VectorXd y;
+};
+
+
+data_size get_data_size(std::string input_filename, char delim) {
+    std::ifstream infile(input_filename);
+
+    if (!infile.is_open()) {
+        std::cerr << "Failed to open " << input_filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    data_size input_sizes;
+    std::string line;
+    std::size_t lc = 0;
+    while (std::getline(infile, line)) {
+        if (line.empty()) {
+            continue;
+
+        }
+
+        if (lc == 0) {
+            input_sizes.n_features = std::count(line.begin(), line.end(), delim);
+        }
+        lc++;
+    }
+    input_sizes.n_examples = lc;
+
+    infile.close();
+    return input_sizes;
+}
+
+
+input_data read_input_file(std::string input_filename, data_size input_sizes, char delim) {
+
+    std::ifstream infile(input_filename);
+
+    if (!infile.is_open()) {
+        std::cerr << "Failed to open " << input_filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    MatrixXd X(input_sizes.n_examples, input_sizes.n_features);
+    VectorXd y(input_sizes.n_examples);
+
+    std::size_t col = 0;
+    std::size_t row = 0;
+    std::string line;
+    while (std::getline(infile, line)) {
+        std::stringstream this_line(line);
+        std::string item;
+
+        while (std::getline(this_line, item, delim)) {
+            if (item.empty()) {
+                continue;
+            }
+
+            if (col == input_sizes.n_features) {
+                y(row) = std::stod(item);
+                col = 0;
+                row++;
+            } else {
+                X(row, col) = std::stod(item);
+                col++;
+            }
+        }
+
+    }
+    infile.close();
+
+    input_data output;
+    output.X = X;
+    output.y = y;
+    return output;
+}
+
+// ----------------
+
+
+
+// ----------------
+// Do the Regression
 
 MatrixXd append_ones_column(MatrixXd X) {
     MatrixXd new_x(X.rows(), X.cols() + 1);
@@ -79,13 +230,11 @@ VectorXd RidgeRegression::predict(MatrixXd xvals) {
         return xvals * this->weights;
     }
 }
+// ----------------
+
 
 
 void print_results(VectorXd weights, std::streamsize prec) {
-
-    const char sep = ' ';
-//    const int width = 5;
-
     std::streamsize curr_prec = std::cout.precision();
     std::cout << std::internal;
     std::cout << "Fitted Model\n";
@@ -106,119 +255,21 @@ void print_results(VectorXd weights, std::streamsize prec) {
 }
 
 
-void print_help_message() {
-    std::cout << "help!" << std::endl;
-    return;
-}
-
-
-struct command_line_args {
-    command_line_args(): lambda(DEFAULT_LAMBDA_VALUE) {}
-    double lambda;
-    std::string input_filename;
-
-};
-
-
-command_line_args parse_args(int argc, char *argv[]) {
-    const char *const short_opts = "l:h";
-    const option long_opts[] = {
-            {"lambda", required_argument, nullptr, 'l'},
-            {"help",   no_argument, nullptr, 'h'},
-    };
-
-
-    command_line_args output;
-    int opt;
-    int option_index = 0;
-    while ((opt = getopt_long(argc, argv, short_opts, long_opts, &option_index)) != -1) {
-        switch (opt) {
-            case 'l':
-                output.lambda = std::stod(optarg);
-                break;
-
-            case 'h':
-            case '?':
-            default:
-                print_help_message();
-                exit(EXIT_SUCCESS);
-        }
-    }
-
-    // sort out the below!!!
-    if (optind < argc) { // IS THIS THE BEST WAY?
-        output.input_filename = argv[optind++];
-    } else {
-        std::cerr << "Failure: must give an input file." << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if (optind < argc){
-        std::cerr << "Warning: "<< argc - optind << " additional arguments given, all ignored!\n" << std::endl;
-    }
-
-    return output;
-}
-
-
-struct input_data {
-    MatrixXd X;
-    VectorXd y;
-};
-
-
-input_data read_input_file(std::string input_filename){
-    input_data inputs;
-
-    std::ifstream infile(input_filename);
-
-    if (!infile.is_open()) {
-        std::cerr << "Failed to open " << input_filename << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    while (std::getline(infile, line)) {
-        std::cout << line << std::endl;
-    }
-
-    // read file
-
-    return inputs;
-}
-
-
 int main(int argc, char *argv[]) {
 
     command_line_args args = parse_args(argc, argv);
-
-
-    int n_examples = 5;
-    double err_size = 0.1;
-
-    VectorXd u = VectorXd::LinSpaced(n_examples, 0, 4);
-    VectorXd u2 = VectorXd::LinSpaced(n_examples, 4, 0);
-
-    MatrixXd X(n_examples, 2 * u.cols());
-    X << u, u2;
-
-    std::srand(0);
-    VectorXd eps = err_size * VectorXd::Random(n_examples);
-    VectorXd y = 2 * u + 3 * u2 + eps;
-
-    std::cout << "X = \n" << X << std::endl;
-    std::cout << "Y = \n" << y << std::endl;
+    data_size dims = get_data_size(args.input_filename, DELIM);
+    input_data data = read_input_file(args.input_filename, dims, DELIM);
 
     RidgeRegression regressor(args.lambda);
-    regressor.fit(X, y, true);
+    regressor.fit(data.X, data.y, true);
 
     VectorXd b = regressor.get_weights();
 
+    VectorXd u = VectorXd::LinSpaced(dims.n_examples, 0, 4);
     MatrixXd xvals = u.replicate(1, 2);
 
-//    std::cout << "beta = \n" << b << std::endl;
-
     VectorXd predictions = regressor.predict(xvals);
-//    std::cout << "predictions = \n" << predictions << std::endl;
 
     std::streamsize prec = 2;
     print_results(b, prec);
@@ -232,9 +283,12 @@ int main(int argc, char *argv[]) {
  * APPARENTLY THE USE OF QR IS MORE COMMON FOR LEAST SQUARES PROBLEMS !!!!!!!!! BALLS
 
  *  Maybe it is easier to assume full rank of the X matrix?
+ *  need ot do something witht the predictions
+ *  also need to take the output prec from the command line args.
  *
  *
  * TODO:
+ * - need to take the adding a constant from the command line args
  * - reading input fule
  * - write usage/help message
  * - use QR rather than cholesky?
